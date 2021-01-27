ac2ed84b622ba7947958f61cf89b2e075af2dbc9
diego manuel guerrero torres <diegomgt11@gmail.com>
2021-01-26 21:08:20 -0500

    Operadores_asignacion_26_01_2021

---
+++ main.js
@@ -0,0 +1,68 @@
+let x =20;
+let y= 30;
+
+console.log("asignacion",x);
+
+// x =  y;
+// console.log(x)
+
+// operradoresde adicion
+ // abrevaicion
+let a =20;
+ let b= 30;
+
+ a += b;
+
+ console.log("asignacion de adicion", a)
+
+// //  signinifcado
+// x = x+y;
+
+// operradoresde sustracion
+ // abrevaicion
+ let m =20;
+ let n= 30;
+ m -= n;
+//  console.log(m)
+// abrevaicion
+//  res = (x-y);
+//  signinifcado
+ console.log("asignacion de sustracion",m);
+
+ let t =20;
+ let c= 30;
+ t *= c;
+//  console.log(m)
+// abrevaicion
+//  res = (x-y);
+//  signinifcado
+ console.log("asignacion de multiplicacion",t);
+
+ let q =16;
+ let f= 8;
+ q /= f;
+//  console.log(m)
+// abrevaicion
+//  res = (x-y);
+//  signinifcado
+ console.log("asignacion de divicion",q);
+
+
+ let r =50;
+ let p= 20;
+ r %= p;
+//  console.log(m)
+// abrevaicion
+//  res = (x-y);
+//  signinifcado
+ console.log("asignacion de resto",r);
+
+ let j =2;
+ let k= 2;
+ j **= k;
+//  console.log(m)
+// abrevaicion
+//  res = (x-y);
+//  signinifcado
+ console.log("asignacion de exponenciacion",j);
+
